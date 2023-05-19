81a0e15e81c540a5c972e03d59b7a1fa7f38cc3d
cowperw00d <9215291029@mail.ru>
2023-05-19 17:42:50 +0300

    test someone

--- Client/src/main/java/org/example/Main.java
+++ Client/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 1234456!");
+        System.out.println("Hello someone!");
     }
     public class Person {
 
