18052e3a3b57bf1e00e558e6dd0a44f77ab3e1c4
sleeter <lfmonsterfeed@gmail.com>
2023-05-19 17:53:12 +0300

    new list

--- Client/src/main/java/org/example/Main.java
+++ Client/src/main/java/org/example/Main.java
@@ -1,23 +1,9 @@
 package org.example;
 
-import static org.example.Main.Product.productMaker;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello someone!");
-        productMaker(7, 9);
-        Sum.printOdin();
-    }
-    public class Product {
-        public static void productMaker(int a, int b){
-            System.out.println(a * b);
-        }
-    }
-    public class Sum {
-        public static int odin = 1;
-        public static void printOdin() {
-            System.out.println(odin);
-        }
     }
 
 }
