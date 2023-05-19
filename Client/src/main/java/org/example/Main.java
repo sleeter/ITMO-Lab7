69628c04868cfb7213a8a1d49c334b5b46c44e12
sleeter <lfmonsterfeed@gmail.com>
2023-05-19 17:07:21 +0300

    modules

--- Client/src/main/java/org/example/Main.java
+++ Client/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello хуир!");
+        System.out.println("Hello world!");
     }
 }
