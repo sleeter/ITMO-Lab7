09eff19ccd0e2240997eb41daaa942d8c7534a7b
sleeter <lfmonsterfeed@gmail.com>
2023-05-19 17:50:06 +0300

    test commit client4

--- Client/src/main/java/org/example/Main.java
+++ Client/src/main/java/org/example/Main.java
@@ -3,8 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello someone!");
+        Sum.printOdin();
     }
-    public class Person {
+    public class Sum {
+        public static int odin = 1;
+        public static void printOdin() {
+            System.out.println(odin);
+        }
+    }
 
-    }
 }
