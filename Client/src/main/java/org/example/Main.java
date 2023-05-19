c71afe436af1be05250dfecb856de4f63d06c132
cowperw00d <9215291029@mail.ru>
2023-05-19 17:50:24 +0300

    test product

--- Client/src/main/java/org/example/Main.java
+++ Client/src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import static org.example.Main.Product.productMaker;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello someone!");
+        productMaker(7, 9);
     }
-    public class Person {
-
+    public class Product {
+        public static void productMaker(int a, int b){
+            System.out.println(a * b);
+        }
     }
 }
